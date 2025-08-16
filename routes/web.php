7b1b0048cb3716671d68412bb3313c715b3943ca
InriOma <inrioma@gmail.com>
2025-08-15 23:39:10 -0600

    agregando las rutas - add routes to Contact Form

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
+Route::get('/', [ContactController::class, 'index'])->name('contact.index');
+Route::post('/guardar-mensaje', [ContactController::class, 'store'])->name('contact.store');
+Route::get('/registros', [ContactController::class, 'registros'])->name('contact.registros');
 
 Route::get('/', function () {
     return view('welcome');
